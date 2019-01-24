9fd0563689333b38873783a68e28fa8167963326
jcliang001 <43047100+jcliang001@users.noreply.github.com>
2019-01-23 22:09:33 -0700

    Update mysh.cpp

--- mysh.cpp
+++ mysh.cpp
@@ -6,15 +6,13 @@
 #include<vector>
 #include <bits/stdc++.h>
 #include<algorithm>
+#include <sys/types.h> 
+#include <sys/wait.h> 
 
 
 using std::cout;
 using std::endl;
-std::vector<std::string> selection{
-    "cd",
-    "exit",
-    "pwd"
-};
+
 static const int LOOP = 1; // without any errors, continue to execute the program.
 static const int EXIT = 0; // when user type exit command in the program then exit safely.
 
@@ -43,7 +41,7 @@ void excutePwdFunction(){
 void excuteCdFunction(std::vector<std::string> &arguments){
     unsigned int numOfElement = arguments.size();
     char* homeDir = getenv("HOME");
-    char* currentDir;
+    
     if(numOfElement == 1){
         if (chdir(homeDir) != 0)
             printStderr();
@@ -90,6 +88,34 @@ int excuteFunction(std::vector<std::string> &arguments){
     
 }
 
+void newProcess(std::vector<std::string> arguments){
+    pid_t pid = -1;
+    pid = fork();
+    //after we initialize the pid, 
+    if(pid == 0){
+        int size = arguments.size();
+        char *argm[size+1];
+        for(unsigned int i=0; i < size; i++){
+            std::strcpy (argm[i], arguments.at(i).c_str());
+        }
+        argm[size] = 0;
+        execvp(argm[0],argm);
+        //if the execvp fail to run, then it will execute the following statement.
+        printStderr();
+        exit(EXIT_FAILURE);
+    }
+    //after we finish the child process, then we need to wait the parent process to finish
+    else if(pid > 0){
+        wait(0);
+        
+    }
+   // if the creation of a child process was unsuccessful, then the pid will return negative number
+   else{
+       printStderr();
+       exit(EXIT_FAILURE);
+   }
+}
+
 
 
 int main(int argc, char** argv){
@@ -97,6 +123,8 @@ int main(int argc, char** argv){
     int status = 1;
     std::vector<std::string> arguments;//store the argument.
     std::string line;
+    
+    std::vector<std::string> selection{"cd","exit","pwd"};
     
     if(argc > 2){
         printStderr();
@@ -128,7 +156,7 @@ int main(int argc, char** argv){
             status = excuteFunction(arguments);
         else{
             status = 1; //if we can't find the command, then we continue the process.
-            printStderr();
+            newProcess(arguments);
         }
             
         
@@ -141,7 +169,7 @@ int main(int argc, char** argv){
         std::ifstream inputFile(fileName);
         if(inputFile.fail()){
             printStderr();
-            exit(0);
+            exit(1);
         }
         while(std::getline(inputFile,line)){
             
@@ -166,7 +194,7 @@ int main(int argc, char** argv){
             status = excuteFunction(arguments);
         else{
             status = 1; //if we can't find the command, then we continue the process.
-            printStderr();
+            newProcess(arguments);
         }
             
         
